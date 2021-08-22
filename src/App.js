28c9ef254947e73fa973a5d34f9c8a032e3aceac
Nicole Eluwa <nicoleeluwa@gmail.com>
2021-08-22 16:21:04 +0000

    Replacing anchor tags with links and adding Routes to Myreads App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react'
+import React from "react";
 // import * as BooksAPI from './BooksAPI'
-import Header from './components/Header'
-import Shelves from './components/Shelves'
-import SearchButton from './components/SearchButton'
-import SearchPage from './components/SearchPage'
-import './App.css'
+import Header from "./components/Header";
+import Shelves from "./components/Shelves";
+import SearchButton from "./components/SearchButton";
+import SearchPage from "./components/SearchPage";
+import "./App.css";
+import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage />
-        ) : (
-          <div className="list-books">
-            <Header />
-       		<Shelves />
-            <SearchButton />
-          </div>
-        )}
+        <Route exact path="/search" render={() => <SearchPage />} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <Header />
+              <Shelves />
+              <SearchButton />
+            </div>
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class SearchButton extends Component {
   render() {
     return (
       <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
+        <Link to="/search">Add a book</Link>
+      </div>
     );
   }
 }
